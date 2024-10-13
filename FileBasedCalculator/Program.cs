26ae810420cb1545f104ea0d0387071dcc2bbaaa
Deryn Cullen <deryn.cullen@sky.uk>
2024-10-13 17:39:56 +0100

    File-based-calculator

--- FileBasedCalculator/Program.cs
+++ FileBasedCalculator/Program.cs
@@ -2,11 +2,9 @@
 
 public class Calculator
 {
-    // Modify these paths if needed
-    // These terms are set to 'public' and 'static' to make them accessible for the Unit Tests
-    public static string f1Path = "C:\\Users\\Public\\Documents\\1.txt";
-    public static string f2Path = "C:\\Users\\Public\\Documents\\2.txt";
-    public static string outputPath = "C:\\Users\\Public\\Documents\\result.txt";
+    public static string f1Path = "./1.txt";
+    public static string f2Path = "./2.txt";
+    public static string outputPath = "./result.txt";
 
     public static void Main(string[] args)
     {
@@ -15,16 +13,30 @@ public class Calculator
         while (true)
         {
             Console.Write("Enter an operator (+, -, /, *): ");
-
-            // Simplified code using TryParse
             if (char.TryParse(Console.ReadLine(), out opr) && "/-+*".Contains(opr))
                 break;
 
             Console.WriteLine("Invalid operator.");
         }
+        string[] numbers_1 = File.ReadAllLines(f1Path);
+        string[] numbers_2 = File.ReadAllLines(f2Path);
+        float result = 0.0f, n1, n2;
+            using StreamWriter writer = new(outputPath);
+            for (int i = 0; i < numbers_1.Length; i++)
+            {
+                n1 = float.Parse(numbers_1[i]);
+                n2 = float.Parse(numbers_2[i]);
 
-        // Write code below this line
+                if (opr == '+') result = n1 + n2;
+                else if (opr == '-') result = n1 - n2;
+                else if (opr == '*') result = n1 * n2;
+                else if (opr == '/') {
+                    if (n2 != 0) result = n1 / n2;
+                    else writer.WriteLine("Cannot divide by zero");
+                    }
 
-        // Write code above this line
+                writer.WriteLine(result);
+            }
+            Console.WriteLine($"Results saved in {outputPath}");
     }
 }
